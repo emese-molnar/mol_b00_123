08e6105e1c0f3543166e36d145b1efa1b4936351
emese-molnar <emolnar04@gmail.com>
2025-02-25 18:49:54 +0100

    Initial commit

--- launch/launch_example1.launch.py
+++ launch/launch_example1.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
             ]
         ),
         # Node(
-        #     package='ros2_cpp_template',
+        #     package='mol_b00_123',
         #     executable='simple_sub_node',
         #     output='screen',
         # ),
